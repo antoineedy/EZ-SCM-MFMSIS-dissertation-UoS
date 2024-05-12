d35765acda3270b663ce84bd17f315fe16c8cbfb
Antoine EDY <antoineedy@outlook.fr>
2024-05-12 15:41:07 +0100

    added some utils

--- start_training.py
+++ start_training.py
@@ -1,5 +1,7 @@
-command = "bash dist_train.sh configs/voc12/vpt_seg_zero_vit-b_512x512_20k_12_10.py data/VOC2012"
 import os
 
-os.system("ls >> test.txt")
+os.chdir("multi-modal-dissertation-uos")
+
+command = "bash dist_train.sh configs/voc12/vpt_seg_zero_vit-b_512x512_20k_12_10.py data/VOC2012"
+
 os.system(command)
