6aa3c34b93c2ff9c333852c3801c53f074be4044
Antoine EDY <antoineedy@outlook.fr>
2024-06-05 09:55:07 +0100

    Refactor Docker image to use pytorch/pytorch

--- start_training.py
+++ start_training.py
@@ -1,7 +1,9 @@
 import os
 
-os.chdir("multi-modal-dissertation-uos")
+# os.chdir("multi-modal-dissertation-uos")
 
-command = "bash dist_train.sh configs/voc12/vpt_seg_zero_vit-b_512x512_20k_12_10.py data/VOC2012"
+os.system("ls >> test.txt")
+
+command = "bash /user/HS400/ae01116/Documents/multi-modal-dissertation-uos/dist_train.sh configs/voc12/vpt_seg_zero_vit-b_512x512_20k_12_10.py data/VOC2012"
 
 os.system(command)
