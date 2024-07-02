2ac03515f5ee1b823a7b65d86603cccdc2bd58ca
Antoine EDY <antoineedy@outlook.fr>
2024-07-03 00:03:13 +0100

    chore: Update schedule_20k.py with evaluation configuration
    Update the evaluation configuration in schedule_20k.py to include the start index, interval, metric, and class indices for both seen and unseen classes. This change ensures that the evaluation process captures the desired classes and intervals accurately.

--- configs/_base_/schedules/schedule_20k.py
+++ configs/_base_/schedules/schedule_20k.py
@@ -6,5 +6,9 @@ lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=20000)
 checkpoint_config = dict(by_epoch=False, interval=50, max_keep_ckpts=20)
-evaluation = dict(start = 100, interval=50, metric='mIoU') #interval == 20001
+
+base_class = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+novel_class = [15, 16, 17, 18, 19]
+
+evaluation = dict(start = 19000, interval=50, metric='mIoU', seen_idx = base_class, unseen_idx = novel_class) #interval == 20001
  
